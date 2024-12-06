e3f0a6c1989bd884d0c074983d23a76455c0ba91
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2024-12-06 14:56:17 +0530

    Update PlayerController Script

--- Catch-Craze/Assets/Scripts/PlayerController.cs
+++ Catch-Craze/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private GameManager gameManager;
+    private Rigidbody2D playerRb;
+    private Vector2 moveInput;
+    public float moveSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerRb.AddForce(moveInput*moveSpeed);
+    }
+    
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
     private void OnCollisionEnter2D(Collision2D other) 
     {
